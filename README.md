5ec8915ccb4684bbd2dc064d67ce079288e6c16a
Yellow-BringtEast <714376214@qq.com>
2023-12-29 22:47:48 +0800

    初始化项目-数据采集模块

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# qingteng_data_analysis
+# 项目说明
+
+## 重要声明
+
+本项目主要基于青藤之恋匿名投票贴评论数据，分析当代年情人赛博相亲时，对有关异地恋、彩礼、性别对立等话题的思考。基于大样本数据，全面剖析当代年轻人的恋爱观和婚姻观。本项目仅供学习交流使用，请勿用于商业用途，否则后果自负。
+
+## 项目介绍
+
+本项目数据基于Mitmproxy中间人攻击和微信PC端小程序抓包获取，所有数据经过脱敏处理，仅包含项目分析所需基本字段，不包含个人隐私信息。
+
+## 项目进度
+
+- [x] 框架搭建
+- [x] 数据获取
+- [x] 数据清洗
+- [ ] 数据分析
+- [ ] 数据可视化
+
+## 数据获取
+
+### 微信小程序抓包
+
+Mitmproxy中间人攻击是一种网络代理工具，可以截获和修改网络请求和响应。
+
+1. 安装Mitmproxy
+
+```bash
+pip install mitmproxy
+```
+
+安装完成后，可以使用`mitmproxy --version`查看是否安装成功。
+![mitmproxy_install](res/mitmproxy_install1.png)
+
+2. 安转mitmproxy-ca证书
+
+- 打开终端，输入以下命令：`mitmproxy`，启动后，会在当前用户目录下生成一个`.mitmproxy`
+  文件夹，进入该文件夹，点击`mitmproxy-ca-cert.cer`文件进行安装。
+
+![mitmproxy_install](res/mitmproxy_install2.png)
+
+![mitmproxy_install](res/mitmproxy_install3.png)
+
+![mitmproxy_install](res/mitmproxy_install4.png)
+
+![mitmproxy_install](res/mitmproxy_install5.png)]
+
+![mitmproxy_install](res/mitmproxy_install6.png)
+
+### 数据获取
+
+1. 进入script目录，在终端中运行`mitmdump -s get_data.py -q`。
+
+2. 开启代理
+
+- 在设置中搜索`代理`，点击代理服务器设置。
+
+![data_crawl](res/data_crawl1.png)
+
+- 点击`手动代理`，输入`127.0.0.1`和`8080`端口，点击`保存`。
+
+![data_crawl](res/data_crawl2.png)
+
+3. 进入微信小程序，点击`村口` -> `村口投票`
+
+![匿名投票贴](res/vote1.png)
+
+4. 点击相关投票贴，滑动到底，即可完成数据采集，请注意控制滑动速度，滑动速度过快会被临时封禁。
+
+5. 采集完成后，关闭终端，关闭手动代理，数据保存到`data`目录下。
+
+## 结语
+
+欢迎您使用本项目对自己感兴趣的话题进行数据分析，如果您有任何问题，欢迎在Issues中联系我。
+
+
---
+++ scripts/get_data.py
@@ -0,0 +1,128 @@
+import json
+import os
+import re
+
+import pandas as pd
+
+null = 'null'
+false = 'false'
+true = 'true'
+
+
+class GetCommentData:
+    def __init__(self):
+        # 评论请求地址
+        self.url_list = ['https://api-love.qingteng-inc.com/moment/get_moment_detail',
+                         'https://api-love.qingteng-inc.com/moment/get_comments_list']
+        self.moment_info_lst = []
+        self.comment_info_lst = []
+
+    def response(self, flow):
+        # 拦截请求，如果为评论请求，则获取评论内容
+        if flow.request.url in self.url_list:
+
+            # 获取投票id
+            res_body = flow.request.get_text()
+            moment_id = re.findall('moment_id=([0-9]+)', res_body)[0]
+
+            # 获取评论内容
+            res = flow.response.content
+            res_json = json.loads(res)
+
+            moment_info, comment_list = self.extract_data(res_json, moment_id)
+
+            if moment_info is not None:
+                self.moment_info_lst.append(moment_info)
+
+            self.comment_info_lst.append(comment_list)
+
+            # 保存数据
+            self.sava_data()
+
+    def sava_data(self):
+        try:
+            df_moment_info = pd.read_csv('../data/moment_info.csv')
+        except FileNotFoundError:
+            df_moment_info = pd.DataFrame()
+
+        try:
+            df_comment_info = pd.read_csv('../data/comment_info.csv')
+        except FileNotFoundError:
+            df_comment_info = pd.DataFrame()
+
+        df_moment = pd.DataFrame(self.moment_info_lst).drop_duplicates(subset=['moment_id'])
+
+        df_comment = pd.DataFrame()
+        for comment_info in self.comment_info_lst:
+            df_temp = pd.DataFrame(comment_info)
+            df_comment = pd.concat([df_comment, df_temp])
+
+        df_moment_info = pd.concat([df_moment_info, df_moment])
+        df_moment_info.drop_duplicates(subset=['moment_id'], inplace=True)
+        df_moment_info.to_csv('../data/moment_info.csv', index=False)
+
+        df_comment_info = pd.concat([df_comment_info, df_comment])
+        df_comment_info.drop_duplicates(inplace=True)
+        df_comment_info.to_csv('../data/comment_info.csv', index=False)
+
+    @staticmethod
+    def extract_data(moment_data, moment_id):
+        moment_data = moment_data['data']
+
+        if moment_data is None:
+            moment_info_data = None
+            comment_info_data = None
+
+        elif 'moment_info' in moment_data.keys():
+            moment_info_data = {
+                'moment_id': moment_data['vote_detail']['vote_info']['id'],
+                'content': moment_data['vote_detail']['vote_info']['content'],
+                'options': moment_data['vote_detail']['vote_info']['options'],
+                'vote_result': moment_data['vote_detail']['vote_result'],
+            }
+
+            comment_info_data = [
+                {
+                    'moment_id': moment_id,
+                    'comment_id': comment_data['comment_id'],
+                    'gender': comment_data['comment_user_info']['gender'],
+                    'age': comment_data['comment_user_info']['age'],
+                    'city': comment_data['comment_user_info']['city'],
+                    'province': comment_data['comment_user_info']['province'],
+                    'profession': comment_data['comment_user_info']['profession'],
+                    'education': comment_data['comment_user_info']['education'],
+                    'content': comment_data['content'],
+                    'reply_num': comment_data['reply_num'],
+                    'thumbs_up_num': comment_data['thumbs_up_num'],
+                    'vote_option_id': comment_data['vote_option_id'],
+                }
+                for comment_data in moment_data['comment_list']
+            ]
+
+        else:
+            moment_info_data = None
+            comment_info_data = [
+                {
+                    'moment_id': moment_id,
+                    'comment_id': comment_data['comment_id'],
+                    'gender': comment_data['comment_user_info']['gender'],
+                    'age': comment_data['comment_user_info']['age'],
+                    'city': comment_data['comment_user_info']['city'],
+                    'province': comment_data['comment_user_info']['province'],
+                    'profession': comment_data['comment_user_info']['profession'],
+                    'education': comment_data['comment_user_info']['education'],
+                    'content': comment_data['content'],
+                    'reply_num': comment_data['reply_num'],
+                    'thumbs_up_num': comment_data['thumbs_up_num'],
+                    'vote_option_id': comment_data['vote_option_id'],
+                }
+                for comment_data in moment_data['comments_list']
+            ]
+
+        return moment_info_data, comment_info_data
+
+
+addons = [GetCommentData()]
+
+if __name__ == '__main__':
+    os.system('mitmdump -s get_data.py -q')
